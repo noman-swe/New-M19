d79a9cb5ca9550695f6354d50b46bacb750de01d
noman-swe <noman.swe130@gmail.com>
2022-07-19 10:02:00 +0600

    Array declaretion & loops

---
+++ recap.js
@@ -0,0 +1,27 @@
+var faboriteBook = 'faborite Book';
+var bookList = ['positioning', 'hooked', 'start with why', 'dogs barking'];
+var dogIndex = bookList.indexOf('dogs barking');
+bookList[1] = 'Story Brand';
+bookList.push('Small giants');
+bookList.push('Big giants');
+bookList.pop();
+console.log(bookList);
+
+//if else
+if(bookList[1] = 'hooked') {
+    console.log('true');
+}else{
+    console.log('Not Hooked');
+}
+
+// while loop
+var i = 0;
+while(i < 6){
+    console.log(i);
+    i++;
+}
+
+// for loop
+for(var i = 0; i < 15; i++){
+    console.log(i);
+}
