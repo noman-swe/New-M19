8f26193b6bf06a05840f03ce96e61a870ae54021
noman-swe <noman.swe130@gmail.com>
2022-07-20 11:19:48 +0600

    19-7 (optional) Javascript switch case break and default

---
+++ switch-case.js
@@ -0,0 +1,18 @@
+var color = 'red';
+
+switch(color){
+    case 'blue':
+        console.log('blue color');
+        break;
+
+        case 'black':
+        console.log('Black Color');
+        break;
+        
+        case 'white':
+            console.log('White Color');
+            break;
+
+            default:
+                console.log('No Color');
+}
