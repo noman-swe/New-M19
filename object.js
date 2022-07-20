8bcc9458a051c4ab8f41b3d541a3862c5f325e38
noman-swe <noman.swe130@gmail.com>
2022-07-20 10:45:29 +0600

    19-5 Declare multiple objects with multiple properties

---
+++ object.js
@@ -0,0 +1,16 @@
+var student1 = {id: 115, name: 'Wafee', marks: 77, phone: 054643212334 };
+
+var mobile = {
+    color:'black',
+    price: 28008,
+    screenSize: 5,
+    storage: '16gb'
+}
+
+var book = {
+    title: 'Start with why',
+    author: 'Simon Sinek',
+    price: 320,
+    pages: 234,
+    publisher: 'penguin'
+}
