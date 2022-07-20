fe6f5faac624c85e3fce50a38808c84172c1d6a5
noman-swe <noman.swe130@gmail.com>
2022-07-20 09:42:47 +0600

    19-2 function declare & call function

---
+++ function.js
@@ -0,0 +1,10 @@
+function turnOnFan (){
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+turnOnFan();
+console.log('eat breakfast');
+turnOnFan();
+console.log('finish the shower');
+turnOnFan();
+
